59d872624641c4c8571b17452bc4b4e7180e3f3a
samucati <samuca.ti@hotmail.com>
2022-06-09 22:14:23 -0300

    First commit
    First commit

---
+++ Proxy/readme.md
@@ -0,0 +1,7 @@
+**Design Pattern Proxy**
+
+Nesta aula, aprendemos:
+
+A interceptar o acesso a um método, através de uma classe derivada;
+Quando isso pode ser útil, em nosso caso, para fazer cache;
+Que, a esse conceito, se dá o nome de Proxy.
---
+++ Proxy/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -0,0 +1,59 @@
+package br.com.alura.loja.orcamento;
+
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Orcamento implements Orcavel {
+    private BigDecimal valor;
+    private SituacaoOrcamento situacao;
+    private List<Orcavel> itens;
+
+    public Orcamento() {
+        this.valor = BigDecimal.ZERO;
+        this.itens = new ArrayList<>();
+        this.situacao = new EmAnalise();
+    }
+
+    public BigDecimal getValor(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return valor;
+    }
+
+    public int getQuantidadeItens(){
+        return itens.size();
+    }
+
+    public void adicionarItem(Orcavel item){
+        this.valor = valor.add(item.getValor());
+        this.itens.add(item);
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+}
